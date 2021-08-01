d57c68bc06de2b65ecb145e3f8140e90afdcfcbe
rjsharma07 <rajattsharma07@gmail.com>
2021-08-01 23:38:27 +0530

    Added logout route in sidenav

--- resources/views/layouts/sidenav.blade.php
+++ resources/views/layouts/sidenav.blade.php
@@ -5,6 +5,7 @@
   <a href="{{route('clients.index')}}">Clients</a>
   <a href="{{route('vendors.index')}}">Vendors</a>
   <a href="{{route('vendordetails.index')}}">Vendors Details</a>
+  <a href="{{route('logout')}}">Logout</a>
 </div>
 
 <!-- Use any element to open the sidenav -->
